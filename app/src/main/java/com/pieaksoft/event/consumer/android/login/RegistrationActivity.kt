4b6bcf9c2a42eb4b5307c5c2b56596277fecac9a
mkylychev <mkylychev@gmail.com>
2021-08-09 19:26:14 +0600

    add registration screen

--- app/src/main/java/com/pieaksoft/event/consumer/android/login/RegistrationActivity.kt
+++ app/src/main/java/com/pieaksoft/event/consumer/android/login/RegistrationActivity.kt
@@ -6,7 +6,7 @@ import com.pieaksoft.event.consumer.android.R
 import com.pieaksoft.event.consumer.android.base.BaseActivity
 import com.pieaksoft.event.consumer.android.utils.newIntent
 
-class RegistrationActivity: BaseActivity(R.layout.activity_registration)  {
+class RegistrationActivity: BaseActivity(R.layout.reg_location)  {
     override fun setView() {
 
     }
