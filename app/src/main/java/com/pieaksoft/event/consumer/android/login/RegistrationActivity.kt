062c1829453a425c58407337a2298b5c2d4ee4dd
mkylychev <mkylychev@gmail.com>
2021-08-10 10:25:06 +0600

    add caountries drop down

--- app/src/main/java/com/pieaksoft/event/consumer/android/login/RegistrationActivity.kt
+++ app/src/main/java/com/pieaksoft/event/consumer/android/login/RegistrationActivity.kt
@@ -2,13 +2,18 @@ package com.pieaksoft.event.consumer.android.login
 
 import android.content.Context
 import android.content.Intent
+import android.widget.ArrayAdapter
 import com.pieaksoft.event.consumer.android.R
 import com.pieaksoft.event.consumer.android.base.BaseActivity
 import com.pieaksoft.event.consumer.android.utils.newIntent
+import kotlinx.android.synthetic.main.reg_location.*
 
 class RegistrationActivity: BaseActivity(R.layout.reg_location)  {
     override fun setView() {
 
+        val countries = resources.getStringArray(R.array.countries)
+        val adapter = ArrayAdapter(this, R.layout.drop_down_item, countries)
+        location_value.setAdapter(adapter)
     }
 
     companion object {
