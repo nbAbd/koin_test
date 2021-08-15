04316dce2094f9e984646ce5eb2d4733f6125c05
mkylychev <mkylychev@gmail.com>
2021-08-15 23:05:06 +0600

    add progress

---
+++ app/src/main/java/com/pieaksoft/event/consumer/android/ui/home/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.pieaksoft.event.consumer.android.ui.home
+
+import com.peaksoft.e_commerce.base.BaseFragment
+import com.pieaksoft.event.consumer.android.R
+
+class HomeFragment: BaseFragment(R.layout.fragment_home) {
+    override fun setViews() {
+
+    }
+
+}
